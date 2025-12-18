e26833a400ad07bf4162f5722854d731e0f5e86b
Nathaniel John Tampus <nat@nize.ph>
2025-12-18 15:04:21 +0800

    chore: remove vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,10 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
-import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     devtools(),
-    tsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
     tanstackStart(),
     // https://tanstack.com/start/latest/docs/framework/react/guide/hosting
     nitro(),
