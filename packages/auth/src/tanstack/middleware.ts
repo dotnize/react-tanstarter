cbc75787ba914edfb8448bcec6cff40f1f13f3ab
Nathaniel John Tampus <nat@nize.ph>
2026-02-05 08:16:59 +0800

    chore: update middleware comments

--- packages/auth/src/tanstack/middleware.ts
+++ packages/auth/src/tanstack/middleware.ts
@@ -6,11 +6,8 @@ import {
 } from "@tanstack/react-start/server";
 import { auth } from "../auth";
 
-// https://tanstack.com/start/latest/docs/framework/react/guide/middleware
-// This is just an example middleware that you can modify and use in your server functions or routes.
-
 /**
- * Middleware to force authentication on server requests (including server functions), and add the user to the context.
+ * Middleware to enforce authentication on server requests (including server functions), and add the user to the context.
  */
 export const authMiddleware = createMiddleware().server(async ({ next }) => {
   const session = await auth.api.getSession({
