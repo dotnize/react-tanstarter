c2173b1aa2b140994ac042c5377c192db95c760a
Nathaniel John Tampus <nat@nize.ph>
2026-02-09 09:37:31 +0800

    chore: update shadcn components

--- src/lib/auth/hooks.ts
+++ src/lib/auth/hooks.ts
@@ -4,7 +4,7 @@ import { authQueryOptions } from "./queries";
 
 // These hooks can be used in route components or any components.
 // They share the same deduped query as beforeLoad/loaders in __root and the _auth layout,
-// so this will not result in unnecessary duplicate calls.
+// so these will not result in unnecessary duplicate calls.
 
 export function useAuth() {
   const { data: user, isPending } = useQuery(authQueryOptions());
