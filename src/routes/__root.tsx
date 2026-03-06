8ae0dcb589219cecb5f87e93e27ed9d61cdc2f60
Nathaniel John Tampus <nat@nize.ph>
2026-03-07 05:55:59 +0800

    chore: enable cookieCache for default authMiddleware, add freshMiddleware for uncached

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,7 +7,7 @@ import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
-import { authQueryOptions, type AuthQueryResult } from "@/lib/auth/queries";
+import type { AuthQueryResult } from "@/lib/auth/queries";
 
 import appCss from "@/styles.css?url";
 
@@ -15,15 +15,11 @@ export const Route = createRootRouteWithContext<{
   queryClient: QueryClient;
   user: AuthQueryResult;
 }>()({
-  beforeLoad: ({ context }) => {
-    // we're using react-query for client-side caching to reduce client-to-server calls, see /src/router.tsx
-    // better-auth's cookieCache is also enabled server-side to reduce server-to-db calls, see /src/lib/auth/auth.ts
-    context.queryClient.prefetchQuery(authQueryOptions());
-
-    // typically we don't need the user immediately in landing pages,
-    // so we're only prefetching here and not awaiting.
-    // for protected routes with loader data, see /_auth/route.tsx
-  },
+  // Typically we don't need the user immediately in landing pages.
+  // For protected routes with loader data, see /_auth/route.tsx
+  // beforeLoad: ({ context }) => {
+  //   context.queryClient.prefetchQuery(authQueryOptions());
+  // },
   head: () => ({
     meta: [
       {
